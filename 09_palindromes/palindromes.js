b524b742b40e1da01d56517cd54de657732dbea2
Henrik Heinonen <hohein@utu.fi>
2022-09-16 00:12:10 +0300

    done thru 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (mjono) {
+    var i = 0;
+    var j = 0;
 
+    while (i < mjono.length / 2) {
+        if (!(/[a-zA-Z]+/.test(mjono.charAt(i)))) {
+            i++;
+            continue;
+        }
+
+        if (!(/[a-zA-Z]+/.test(mjono.charAt(mjono.length - 1 - j)))) {
+            j++;
+            continue;
+        }
+
+        if (mjono.charAt(i).toLowerCase() !== mjono.charAt(mjono.length - 1 - j)) {
+            console.log(i + " " + j)
+            console.log(mjono.charAt(i).toLowerCase() + " " + mjono.charAt(mjono.length - 1 - j))
+            return false;
+        }
+        i++;
+        j++;
+    }
+    return true;
+    
+    
 };
 
+console.log(palindromes('Racecar!'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 // Do not edit below this line
 module.exports = palindromes;
